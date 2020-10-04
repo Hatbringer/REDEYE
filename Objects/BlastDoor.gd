79f6e1279fd29c710d102fc4a824bb43312eed00
Hatbringer <jacobscottgibson@protonmail.com>
2020-10-04 15:40:40 -0700

    Added Blastdoor

---
+++ Objects/BlastDoor.gd
@@ -0,0 +1,10 @@
+extends Spatial
+
+onready var Sensors = $Area
+onready var Player = get_tree().current_scene.find_node("Player")
+
+func _physics_process(delta):
+	if Sensors.overlaps_body(Player):
+		$KinematicBody.translation.y -= ($KinematicBody.translation.y + 3.9) / 10
+	else:
+		$KinematicBody.translation.y /= 1.1
